6eec014ce78b0f57b2f72ba44aee6fb0588e092e
k1LoW <k1lowxb@gmail.com>
2014-03-11 02:07:59 +0900

    remove unnecessary options

--- centos.sh
+++ centos.sh
@@ -16,7 +16,7 @@ cd $DEVLOCAL/source
 curl -L -o $DEVLOCAL/source/zsh.tar.gz "http://downloads.sourceforge.net/project/zsh/zsh/5.0.5/zsh-5.0.5.tar.gz?r=&ts=1394454707&use_mirror=jaist"
 tar xzvf zsh.tar.gz
 cd zsh-5.0.5
-./configure --prefix=$DEVLOCAL --enable-cflags="-I$DEVLOCAL/include" --enable-cppflags="-I$DEVLOCAL/include" --enable-ldflags="-L$DEVLOCAL/lib"  --enable-multibyte --enable-locale
+./configure --prefix=$DEVLOCAL --enable-multibyte --enable-locale
 make
 make install
 
