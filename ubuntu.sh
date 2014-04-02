bedecbf2fd03c488844c1c602805b83c8fc5fb2c
k1LoW <k1lowxb@gmail.com>
2014-04-02 10:33:33 +0900

    Update

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get -y install ssh expat zlib1g zlib1g-dev tk python gettext libncurses5-dev m4 automake
+sudo apt-get -y install ssh expat zlib1g zlib1g-dev tk python gettext libncurses5-dev m4 automake libcurl4-openssl-dev
