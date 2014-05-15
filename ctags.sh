c96635bb5651eee4d645ae9706273f95eb9337aa
k1LoW <k1lowxb@gmail.com>
2014-05-15 15:18:23 +0900

    update ctags.sh

--- ctags.sh
+++ ctags.sh
@@ -4,7 +4,7 @@ DEVLOCAL=$HOME/.devlocal
 cd $DEVLOCAL/source
 curl -L -o $DEVLOCAL/source/ctags.tar.gz "http://hp.vector.co.jp/authors/VA025040/ctags/downloads/ctags-5.8j2.tar.gz"
 tar xzvf ctags.tar.gz
-cd ctags-5.8j2.tar.gz
+cd ctags-5.8j2
 ./configure --prefix=$DEVLOCAL
 make
 make install
