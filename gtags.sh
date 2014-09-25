faca4a4ff081bdd3348fdce7e679f88d5ceffc7c
k1LoW <k1lowxb@gmail.com>
2014-09-25 13:33:32 +0900

    Update

--- gtags.sh
+++ gtags.sh
@@ -5,6 +5,6 @@ cd $DEVLOCAL/source
 curl -L -o $DEVLOCAL/source/global.tar.gz http://tamacom.com/global/global-6.3.2.tar.gz
 tar xvf global.tar.gz
 cd global-6.3.2
-./configure --prefix=$DEVLOCAL --without-x
+./configure --prefix=$DEVLOCAL
 make
 make install
