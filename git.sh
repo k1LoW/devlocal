d510c785d7c0bf893661631d9667957f3c4c9e75
k1LoW <k1lowxb@gmail.com>
2015-10-14 10:18:03 +0900

    Update git version

--- git.sh
+++ git.sh
@@ -2,9 +2,9 @@
 DEVLOCAL=$HOME/.devlocal
 
 cd $DEVLOCAL/source
-curl -L -o $DEVLOCAL/source/git.tar.gz "https://git-core.googlecode.com/files/git-1.9.0.tar.gz"
+curl -L -o $DEVLOCAL/source/git.tar.gz "https://git-core.googlecode.com/files/git-2.6.1.tar.gz"
 tar xzvf git.tar.gz
-cd git-1.9.0
+cd git-2.6.1
 ./configure --prefix=$DEVLOCAL
 make
 make install
