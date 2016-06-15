c277d86820c27dc882f0823c863a3b293ca4fe8c
Ken’ichiro Oyama <k1lowxb@gmail.com>
2016-06-15 16:58:09 +0900

    Update emacs.sh

--- emacs.sh
+++ emacs.sh
@@ -8,7 +8,7 @@ sudo yum install ncurses-devel
 # emacs
 cd $DEVLOCAL/source
 curl -L -o $DEVLOCAL/source/emacs.tar.gz "http://ftp.gnu.org/pub/gnu/emacs/emacs-24.5.tar.gz"
-zxvf emacs-24.5.tar.gz
+tar zxvf emacs.tar.gz
 cd emacs-24.5/
 ./configure --prefix=$DEVLOCAL --without-x
 make
