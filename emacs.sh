06ae5b943871b1d0e505ba52e65fa32704951fde
k1LoW <k1lowxb@gmail.com>
2018-03-22 23:21:49 +0900

    Add --with-gnutls

--- emacs.sh
+++ emacs.sh
@@ -9,6 +9,6 @@ cd $DEVLOCAL/source
 curl -L -o $DEVLOCAL/source/emacs.tar.gz "http://ftp.gnu.org/pub/gnu/emacs/emacs-$EMACS_VERSION.tar.gz"
 tar zxvf emacs.tar.gz
 cd emacs-$EMACS_VERSION/
-./configure --prefix=$DEVLOCAL --without-x
+./configure --prefix=$DEVLOCAL --without-x --with-gnutls
 make
 make install
